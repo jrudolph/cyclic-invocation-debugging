77f08c848b300874971015833d6295f7955167dd
Johannes Rudolph <johannes.rudolph@gmail.com>
2016-01-26 14:21:52 +0100

    latest

--- src/main/scala/example/4cyclicFutures.scala
+++ src/main/scala/example/4cyclicFutures.scala
@@ -1,34 +1,47 @@
 package example
 
+import scala.concurrent.{ ExecutionContext, Promise, Future }
+
+object Cache {
+  def apply(f: Int ⇒ Future[Int])(implicit ec: ExecutionContext): Int ⇒ Future[Int] = {
+    var cache = Map.empty[Int, Future[Int]]
+    val lock = new Object
+
+    key ⇒
+      lock.synchronized {
+        cache.get(key) match {
+          // cache hit
+          case Some(result) ⇒ result
+          // cache miss
+          case None ⇒
+            val result = Promise[Int]()
+            Future { // run `f` concurrently
+              result.completeWith(f(key))
+            }
+            cache += key -> result.future
+            result.future
+        }
+      }
+  }
+}
+
 object CyclicFutures {
-  import scala.concurrent.{ Promise, Future }
   import scala.concurrent.ExecutionContext.Implicits.global
 
-  def futurePlus1(underlying: ⇒ Future[Int]): Future[Int] = {
-    // we make a few verrenkungen to avoid evaluating `underlying`
-    // too eagerly
-    val p = Promise[Int]()
-    Future {
-      Thread.sleep(100)
-      p.completeWith(underlying)
+  val f: Int ⇒ Future[Int] = Cache(calculateF)
+  def calculateF(i: Int): Future[Int] =
+    i match {
+      case 42 ⇒ f(23).map(_ + 1)
+      case 23 ⇒ f(42).map(_ * 1)
+      case x  ⇒ Future.successful(123)
     }
-    p.future
-  }
-
-  object Results {
-    val a: Future[Int] = futurePlus1(b)
-    val b: Future[Int] = futurePlus1(a)
-  }
 
   def main(args: Array[String]): Unit = {
-    // start computations by initializing `Results`
-    Results
-
     val sum: Future[Int] =
       for {
-        a ← Results.a
-        b ← Results.b
-      } yield a + b
+        aRes ← f(23)
+        bRes ← f(42)
+      } yield aRes + bRes
 
     println("Waiting for completion....")
 
